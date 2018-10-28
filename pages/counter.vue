28d5f876b8c9667adac801b73620524f53511bbc
wasanx25 <wataru.kikuchi0225@gmail.com>
2018-10-28 17:29:57 +0900

    Add counter only increment

---
+++ pages/counter.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <button @click="$store.commit('counter/increment')">{{ counter }}</button>
+  </div>
+</template>
+
+<script>
+import { mapMutations } from "vuex"
+
+export default {
+  computed: {
+    counter() {
+      return this.$store.state.counter.counter
+    }
+  }
+}
+</script>
---
+++ store/counter.js
@@ -0,0 +1,9 @@
+export const state = () => ({
+  counter: 1
+})
+
+export const mutations = {
+  increment(state) {
+    state.counter++
+  }
+}
