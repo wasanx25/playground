3617f9c5c5b9c8c2b22116b5749221696c79bbd3
wasanx25 <wataru.kikuchi0225@gmail.com>
2018-10-28 17:29:10 +0900

    Add my layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,31 @@
 <template>
   <div>
-    <nuxt/>
+    <div id="header" class="container">
+      <h1>wasanx25 playground</h1>
+    </div>
+    <div id="main" class="container">
+      <nuxt/>
+    </div>
+    <div id="footer" class="container">
+      <p>Copyright © 2018 wasanx25 All Right Reserved.</p>
+    </div>
   </div>
 </template>
 
 <style>
+#header {
+  text-align: center;
+}
+
+#main {
+  height: 80vh;
+}
+
+#footer {
+  text-align: center;
+}
+
+#footer p {
+  color: #9e9e9e;
+}
 </style>
