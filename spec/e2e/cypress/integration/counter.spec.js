d82955dd70c625783deaddf438868509ffe620b4
wasanx25 <wataru.kikuchi0225@gmail.com>
2018-10-28 19:25:24 +0900

    Add counter e2e spec

---
+++ spec/e2e/cypress/integration/counter.spec.js
@@ -0,0 +1,53 @@
+describe('/counter', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/counter')
+  })
+
+  it('increments counter and resets', () => {
+    // three counter
+    cy.get('#counter_content > button:nth-child(4)')
+      .contains('+')
+      .click()
+      .click()
+      .click()
+    cy.get('#counter_content > p').should('have.text', '3')
+
+    // thirty counter
+    cy.get('#counter_content > button:nth-child(5)')
+      .contains('+10')
+      .click()
+      .click()
+      .click()
+    cy.get('#counter_content > p').should('have.text', '33')
+  })
+
+  it('decrements counter and resets', () => {
+    // three counter
+    cy.get('#counter_content > button:nth-child(2)')
+      .contains('-')
+      .click()
+      .click()
+      .click()
+    cy.get('#counter_content > p').should('have.text', '-3')
+
+    // thirty counter
+    cy.get('#counter_content > button:nth-child(1)')
+      .contains('-10')
+      .click()
+      .click()
+      .click()
+    cy.get('#counter_content > p').should('have.text', '-33')
+  })
+
+  it('resets counter', () => {
+    // thirty counter before resets
+    cy.get('#counter_content > button')
+      .contains('+10')
+      .click()
+      .click()
+      .click()
+
+    cy.get('#reset').click()
+    cy.get('#counter_content > p').should('have.text', '0')
+  })
+})
