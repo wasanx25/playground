c5fbc21fbb78f01deffc8c1962ce3834459e9555
wasanx25 <wataru.kikuchi0225@gmail.com>
2018-10-28 19:25:38 +0900

    Remove unused spec

--- spec/e2e/cypress/integration/sample.spec.js
+++
@@ -1,9 +0,0 @@
-describe('blogを開く', () => {
-  before(() => {
-    cy.visit('https://blog.wasanx25.com/')
-  })
-
-  it('タイトルが正常', () => {
-    cy.get('#header > h1').contains('wasanx25 blog').should('not.be.hidden')
-  })
-})
