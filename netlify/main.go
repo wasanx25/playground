e231911f385eb85d2d4caf9e879075e9f24498a2
wasanx25 <wataru.kikuchi0225@gmail.com>
2018-11-03 22:54:21 +0900

    Add log in Netlify Functions

--- netlify/main.go
+++ netlify/main.go
@@ -2,8 +2,9 @@ package main
 
 import (
 	"bytes"
+	"fmt"
+	"io/ioutil"
 	"net/http"
-	"net/url"
 	"os"
 
 	"github.com/aws/aws-lambda-go/events"
@@ -11,21 +12,16 @@ import (
 )
 
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	u := url.URL{
-		Scheme: "https",
-		Host:   "api.travis-ci.org",
-		Path:   "repo/wasanx25%2Fplayground/requests",
-	}
-
 	body := `{"request":{"branch":"master"}}`
 
 	req, err := http.NewRequest(
 		"POST",
-		u.String(),
+		"https://api.travis-ci.org/repo/wasanx25%2Fplayground/requests",
 		bytes.NewBuffer([]byte(body)),
 	)
 
 	if err != nil {
+		fmt.Println(err)
 		return events.APIGatewayProxyResponse{
 			StatusCode: 500,
 			Body:       err.Error(),
@@ -40,12 +36,22 @@ func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 	client := &http.Client{}
 	res, err := client.Do(req)
 	if err != nil {
+		fmt.Println(err)
+		return events.APIGatewayProxyResponse{
+			StatusCode: 500,
+			Body:       err.Error(),
+		}, nil
+	}
+	resBody, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
 		return events.APIGatewayProxyResponse{
 			StatusCode: 500,
 			Body:       err.Error(),
 		}, nil
 	}
 	defer res.Body.Close()
+	fmt.Println("Travis Calling Status: " + res.Status + "\nTravis Calling Message: " + string(resBody))
 
 	return events.APIGatewayProxyResponse{
 		StatusCode: 200,
