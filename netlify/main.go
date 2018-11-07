b3f84857a6ac8bf078cc3a57f3531a9bec69d779
wasanx25 <wataru.kikuchi0225@gmail.com>
2018-11-07 09:35:43 +0900

    Complete travis build

--- netlify/main.go
+++ netlify/main.go
@@ -1,62 +1,28 @@
 package main
 
 import (
-	"bytes"
+	"context"
 	"fmt"
-	"io/ioutil"
-	"net/http"
 	"os"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	travis "github.com/shuheiktgw/go-travis"
 )
 
-func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	body := `{"request":{"branch":"master"}}`
-
-	req, err := http.NewRequest(
-		"POST",
-		"https://api.travis-ci.org/repo/wasanx25%2Fplayground/requests",
-		bytes.NewBuffer([]byte(body)),
-	)
+func handler(request events.APIGatewayProxyRequest) {
+	client := travis.NewClient(travis.ApiOrgUrl, os.Getenv("TRAVIS_TOKEN"))
+	req := &travis.RequestBody{
+		Branch: "master",
+	}
+	_, res, err := client.Requests.CreateByRepoSlug(context.Background(), "wasanx25/playground", req)
 
 	if err != nil {
 		fmt.Println(err)
-		return events.APIGatewayProxyResponse{
-			StatusCode: 500,
-			Body:       err.Error(),
-		}, nil
+		return
 	}
 
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("Accept", "application/json")
-	req.Header.Set("Travis-API-Version", "3")
-	req.Header.Set("Autorization", "token "+os.Getenv("TRAVIS_TOKEN"))
-
-	client := &http.Client{}
-	res, err := client.Do(req)
-	if err != nil {
-		fmt.Println(err)
-		return events.APIGatewayProxyResponse{
-			StatusCode: 500,
-			Body:       err.Error(),
-		}, nil
-	}
-	resBody, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		fmt.Println(err)
-		return events.APIGatewayProxyResponse{
-			StatusCode: 500,
-			Body:       err.Error(),
-		}, nil
-	}
-	defer res.Body.Close()
-	fmt.Println("Travis Calling Status: " + res.Status + "\nTravis Calling Message: " + string(resBody))
-
-	return events.APIGatewayProxyResponse{
-		StatusCode: 200,
-		Body:       "Success Travis Build!",
-	}, nil
+	fmt.Println(res)
 }
 
 func main() {
